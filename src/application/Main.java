5ed4fe523af133acb1b10be37893d969f8c0c177
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-07-04 17:35:19 -0300

    adds deletion implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,10 +7,11 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println("===== TEST 1: seller find by Id ======");
         Seller seller = sellerDao.findById(3);
@@ -30,5 +31,16 @@ public class Main {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+        System.out.println("\n===== TEST 5: seller update ======");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+        System.out.println("\n===== TEST 6: seller delete ======");
+        System.out.println("Enter id for deletion");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Deletion completed!");
+        sc.close();
     }
 }
