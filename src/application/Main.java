c58b48becd9a96aa08e7272be1993b977d422897
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-07-04 15:27:44 -0300

    adds find by id implementation to seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,15 @@
 package application;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
-        Department books = new Department(1, "Books");
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
-        Seller bob = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, books);
-        System.out.println(bob);
+        System.out.println(seller);
     }
 }
